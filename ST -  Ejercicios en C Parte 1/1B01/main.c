515bcfd93b735a4667b8d967b56c9107454deb39
Lucas Sanchez <88208727+StudentLucas@users.noreply.github.com>
2022-07-09 01:53:43 -0300

    Delete ST -  Ejercicios en C Parte 1 directory

--- ST -  Ejercicios en C Parte 1/1B01/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-    printf("Primavera Verano Oto\xa4o Invierno\n\n");
-    return 0;
-}
--- ST -  Ejercicios en C Parte 1/1B03/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-    printf("Primavera \nVerano \nOto\xa4o \nInvierno\n\n");
-    return 0;
-}
